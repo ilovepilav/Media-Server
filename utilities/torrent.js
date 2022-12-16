d47975f7bb79e7e4b4f101878840c3df3f08b47c
Çağrı Sakaoğlu <cagrisakaoglu@gmail.com>
2022-12-17 01:45:11 +0300

    torrent commands added

--- utilities/torrent.js
+++ utilities/torrent.js
@@ -2,6 +2,46 @@ import * as util from 'util'
 import child_process from 'child_process'
 const exec = util.promisify(child_process.exec);
 
+export const startTorrentDaemon = async () => {
+  return await commandExec('transmission-daemon')
+}
+
+export const checkDaemonRunning = async () => {
+  const result = await commandExec('pgrep transmission-da')
+  if (!result) return false
+  if (!result[0]) return false
+  if (!isNan(result[0])) return false
+  return true
+}
+
+export const listTorrents = async () => {
+  return await commandExec('transmission-remote -l')
+}
+
+export const addTorrent = async (magnetUrl, dirPath) => {
+  return await commandExec(`transmission-remote -a "${magnetUrl}" -w ${dirPath}`)
+}
+
+export const stopTorrent = async (torrentId) => {
+  return await commandExec(`transmission-remote -t ${torrentId} --remove`)
+}
+
+export const stopAllTorrent = async () => {
+  return await commandExec('transmission-remote -t all --remove')
+}
+
+export const pauseTorrent = async (torrentId) => {
+  return await commandExec(`transmission-remote -t ${torrentId} --stop`)
+}
+
+export const pauseAllTorrent = async () => {
+  return await commandExec('transmission-remote -t all --stop')
+}
+
+export const startAllTorrent = async () => {
+  return await commandExec('transmission-remote -t all --start')
+}
+
 export const commandExec = async (command) => {
   try {
     const { stdout, stderr } = await exec(command);
